1eb1122a40071d3bfb4a392083ae2681ce0a28d5
Work Vince <fung.vin@gmail.com>
2016-11-17 20:36:07 +0800

    added some css

--- db/migrate/20161117025053_rename_column_user.rb
+++
@@ -1,7 +0,0 @@
-class RenameColumnUser < ActiveRecord::Migration
-  def change
-    rename_column :users, :name,  :first_name
-    rename_column :users, :nickname,  :last_name
-    rename_column :users, :image,  :profile_picture
-  end
-end
